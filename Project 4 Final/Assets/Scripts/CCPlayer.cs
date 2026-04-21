709cbc7ae23f66125f04ce7a80c10080606f8b7c
coltergeist.xp <cbubbico@pratt.edu>
2026-04-21 15:23:46 -0400

    Made character controller and started working on randomization

--- Project 4 Final/Assets/Scripts/CCPlayer.cs
+++ Project 4 Final/Assets/Scripts/CCPlayer.cs
@@ -1,16 +1,125 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CCPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    public float walkSpeed = 5;
+    public float runSpeed = 9;
+    public float jumpHeight = 5;
+
+    public Transform cameraTransform;
+    public float lookSensitivity = 1;
+
+    private CharacterController cc;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private float verticalVelocity; //current upward/downward speed
+    private float gravity = -20; //constant downward acceleration
+    private float pitch; //up and down
+
+    public bool isRunning;
+    public bool isJumping;
+    private void Awake()
     {
-        
+        cc = GetComponent<CharacterController>();
+
+        //optional cursor locking
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleLook();
+        HandleMovement();
+    }
+
+    private void HandleLook()
+    {
+        //horizontal mouse movement rotates player
+        float yaw = lookInput.x * lookSensitivity;
+        //vertical mouse movement rotates camera
+        float pitchDelta = lookInput.y * lookSensitivity;
+
+        transform.Rotate(Vector3.up * yaw);
+
+        //accumulate vertical rotation
+        pitch -= pitchDelta;
+        //clamp so we don't flip upside down
+        pitch = Mathf.Clamp(pitch, -90, 90);
+
+        cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
+    }
+
+    private void HandleMovement()
+    {
+        //updating our bool to be true or false if the player is grounded
+        bool grounded = cc.isGrounded;
+        //Debug.Log("Is Grounded: " + grounded);
+
+        //this keeps character controller snapped to ground
+        if (grounded && verticalVelocity <= 0)
+        {
+            verticalVelocity = -2;
+        }
+
+        float currentSpeed = walkSpeed;
+
+        //if running is true set the current speed to run speed
+        if (isRunning)
+        {
+            currentSpeed = runSpeed;
+        }
+        else //if false, set back to walk speed
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        Vector3 move = transform.right * moveInput.x * currentSpeed + transform.forward * moveInput.y * currentSpeed;
+
+        //if jumping is true and we are grounded
+        if (isJumping && grounded)
+        {
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        //apply gravity to every frame
+        verticalVelocity += gravity * Time.deltaTime;
+        //convert vertical velocity into movement vector
+        Vector3 velocity = Vector3.up * verticalVelocity;
+        //NOW we are finally moving player
+        cc.Move((move + velocity) * Time.deltaTime);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //when the key is hit, isJumping = true
+        //can only jump if the menu isn't open (prevents jumping after space is pressed in the menu and then exiting)
+        if (context.performed && Cursor.lockState == CursorLockMode.Locked) isJumping = true;
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        isRunning = context.ReadValueAsButton();
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Debug.Log("CC collided with: " + hit.gameObject.name);
     }
 }
---
+++ Project 4 Final/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [Header("Prefabs")]
+    public GameObject[] prefabs;
+
+    public Transform endPlatform;
+    private List<GameObject> _emptyPlatforms;
+    private List<GameObject> _platforms;
+    private bool[] _level;
+
+    private void Awake()
+    {
+        int tempRandom;
+        _level = new bool[Random.Range(3, 9)];
+        for (int i = 0; i < _level.Length; i++)
+        {
+            if( i == 0 || i == _level.Length - 1 )
+            {
+                _level[i] = true;
+            }
+            else if (_level[i - 1] == false)
+            {
+                _level[i] = true;
+            }
+            else
+            {
+                tempRandom = Random.Range(0, 2);
+                if (tempRandom == 0) _level[i] = false;
+                else _level[i] = true;
+            }
+            Debug.Log(_level[i]);
+        }
+        
+        foreach(GameObject platform in prefabs)
+        {
+            if (platform == null) return;
+            if(platform.tag == "PlatformEmpty")
+            {
+                _emptyPlatforms.Add(platform);
+            } 
+            else if (platform.tag == "Platform")
+            {
+                _platforms.Add(platform);
+            }
+        }
+    }
+}
